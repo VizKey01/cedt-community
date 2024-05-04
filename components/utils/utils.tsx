38f8e3c1c73122a20d64c79b0988112bb3fc63b4
HRNPH <hrnph@protonmail.com>
2024-05-04 08:11:46 +0700

    feat: don't wanna use &apos

--- components/utils/utils.tsx
+++ components/utils/utils.tsx
@@ -3,7 +3,7 @@ export function ComingSoon() {
     <div className="text-center">
       <h1 className="text-6xl text-primary font-bold mb-2">Coming Soon!</h1>
       <p className="text-secondaryCutoff-900 text-xl">
-        We're working hard! (OK... Maybe not that hard) to finish the
+        We are working hard! (OK... Maybe not that hard) to finish the
         development of this site. Stay tuned!
       </p>
     </div>
