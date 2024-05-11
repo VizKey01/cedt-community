17a4630b330ababb0cd01bd4af6a96f04dc94eac
HRNPH <hrnph@protonmail.com>
2024-05-11 15:52:36 +0700

    feat: clean code refractoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,12 @@
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
-import { button as buttonStyles, tv } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { GithubIcon, Logo } from "@/components/icons";
-import { pageWording } from "@/config/wording";
 import ExploreCards from "@/components/home/explore";
-import { SubTitlesTypewriter } from "@/components/home/hero";
+import HeroSection from "@/components/home/hero";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <div className="flex justify-center m-0 p-0">
-          <Logo size={125} />
-        </div>
-        <span className="flex gap-0 justify-center">
-          <h1 className="text-primary">{pageWording.home.heroTitle1}&nbsp;</h1>
-          <h1 className="text-inherit">{pageWording.home.heroTitle2}&nbsp;</h1>
-        </span>
-        <SubTitlesTypewriter />
-      </div>
-
-      <div className="flex gap-3 hover:scale-110 transform transition-transform duration-300 ease-in-out">
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8 m-0 text-sm md:text-base">
-        <Snippet hideSymbol hideCopyButton variant="bordered" className="p-0">
-          <span className="p-0">
-            Welcome to one of the best{" "}
-            <Code color="primary">Computer Major</Code>
-          </span>
-        </Snippet>
-      </div>
-
+      <HeroSection />
       {/* Explore CEDT Section */}
       <ExploreCards />
     </section>
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { pageWording } from "@/config/wording";
 import Typewriter from "typewriter-effect";
+import { Logo } from "../icons";
+import { Code } from "@nextui-org/code";
+import { Snippet } from "@nextui-org/snippet";
 
-export function SubTitlesTypewriter() {
+function SubTitlesTypewriter() {
   return (
     // lock default height, so the page doesn't jump when the text changes
     <h3 className="text-inherit min-h-32 md:min-h-20">
@@ -16,5 +19,41 @@ export function SubTitlesTypewriter() {
         }}
       />
     </h3>
+  );
+}
+
+function QuoteSnippet() {
+  return (
+    <div className="mt-8 m-0 text-sm md:text-base">
+      <Snippet hideSymbol hideCopyButton variant="bordered" className="p-0">
+        <span className="p-0">
+          Welcome to one of the best <Code color="primary">Computer Major</Code>
+        </span>
+      </Snippet>
+    </div>
+  );
+}
+
+function LogoAndWelcomeMessage() {
+  return (
+    <div className="inline-block max-w-lg text-center justify-center">
+      <div className="flex justify-center m-0 p-0">
+        <Logo size={125} />
+      </div>
+      <span className="flex gap-0 justify-center">
+        <h1 className="text-primary">{pageWording.home.heroTitle1}&nbsp;</h1>
+        <h1 className="text-inherit">{pageWording.home.heroTitle2}&nbsp;</h1>
+      </span>
+      <SubTitlesTypewriter />
+    </div>
+  );
+}
+
+export default function HeroSection() {
+  return (
+    <>
+      <LogoAndWelcomeMessage />
+      <QuoteSnippet />
+    </>
   );
 }
