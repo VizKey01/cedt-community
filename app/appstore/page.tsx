7335b25631512ecf05abe0187cf384f33ec61413
VizKey01 <micmin0101@gmail.com>
2024-05-25 00:00:48 +0700

    First setup Appstore

---
+++ app/appstore/page.tsx
@@ -0,0 +1,10 @@
+import { ComingSoon } from "@/components/utils/utils";
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1 className={""}>CEDT Appstore</h1>
+      <ComingSoon />
+    </div>
+  );
+}
--- config/site.ts
+++ config/site.ts
@@ -17,6 +17,10 @@ export const siteConfig = {
 			href: "/internship",
 		},
 		{
+			label: "App",
+			href: "/appstore",
+		},
+		{
 			label: "About",
 			href: "/about",
 		}
@@ -33,6 +37,10 @@ export const siteConfig = {
 		{
 			label: "Intern",
 			href: "/internship",
+		},
+		{
+			label: "App",
+			href: "/appstore",
 		},
 		{
 			label: "About",
